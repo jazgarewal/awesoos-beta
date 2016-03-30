41d717fab16b3754a5fe14ad1fb508d158bac287
Jaz Garewal <jaz.garewal@savvyapps.com>
2016-03-29 21:45:20 -0700

    Added twilio cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,23 @@
+var secret = process.env.APP_SECRET;
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var authToken = process.env.TWILIO_AUTH_TOKEN;
+var sendingNumber = process.env.TWILIO_NUMBER;
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
+});
+
+Parse.Cloud.define('sendAMessage', function(req, res) {
+	Parse.Cloud.httpRequest({
+		method: "POST",
+		url:"https://" + accountSid + ":" + authToken + "@api.twilio.com:8443/2010-04-01/Accounts/" + accountSid + "/Messages.json",
+		to:"+15209557918",
+		From:"+15209065667",
+		Body:"Test"
+	}).then(function(httpResponse) {
+		console.log(httpResponse.text);
+	},
+	function(httpResponse) {
+		console.log(httpResponse.text);
+	});
 });
