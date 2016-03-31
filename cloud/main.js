d48f33619cd4bd65d5a5cfd380a28c0d3d89e4f3
Jaz Garewal <jaz.garewal@savvyapps.com>
2016-03-30 19:42:09 -0700

    Modified twilio test message to use example

--- cloud/main.js
+++ cloud/main.js
@@ -2,12 +2,12 @@ var secret = process.env.APP_SECRET;
 var accountSid = process.env.TWILIO_ACCOUNT_SID;
 var authToken = process.env.TWILIO_AUTH_TOKEN;
 var sendingNumber = process.env.TWILIO_NUMBER;
-var twilio = require("twilio")('accountSid', 'authToken');
+var client = require("twilio")('accountSid', 'authToken');
 
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success("https://" + accountSid + ":" + authToken + "@api.twilio.com:8443/2010-04-01/Accounts/" + accountSid + "/Messages.json");
-  
+
 });
 
 Parse.Cloud.define('sendAMessage', function(req, res) {
@@ -26,12 +26,24 @@ Parse.Cloud.define('sendAMessage', function(req, res) {
 });
 
 Parse.Cloud.define('sendATestMessage', function(req, res){
-  twilio.sendMessage({
-    From: "+12677056297",
-    To: "+15209065667",
-    Body: "Test message"
-  }, {
-    success: function(httpResponse) {response.success("Sent!");},
-    error: function(httpResponse) {response.error("Error!");}
-  });
+  client.sendMessage({
+
+    to: '+12677056297', // Any number Twilio can deliver to
+    from: '+15209065667', // A number you bought from Twilio and can use for outbound communication
+    body: 'word to your mother.' // body of the SMS message
+
+}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+    if (!err) { // "err" is an error received during the request, if any
+
+        // "responseData" is a JavaScript object containing data received from Twilio.
+        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+        console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
+
+    }
+
+});
 });
